879db836ea6e9b8b3fb4e5ef534b179080fab347
ylcho9989 <ylcho9989@naver.com>
2024-08-16 23:50:13 +0900

    4주차 django 개발시작

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
+    usable_password = None
     email = forms.EmailField()
     class Meta:
         model = User
